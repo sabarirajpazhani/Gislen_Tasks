efe91e85a56c260ddcc63e065065ed9bbc017aa8
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-27 19:14:20 +0530

    Add project files.

---
+++ Gislen_Tasks/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Gislen_Tasks
+{
+    public class Program {
+        public static void Program_Five()
+        {
+            int number = 100;
+
+            for(int i=1; i<=number; i++)
+            {
+                if(i%3==0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Gislen Software");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Gislen");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Spftware");
+                }
+                else if(i%7==0)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static void Main(String[] args)
+        {
+            Program_Five();
+        }
+    }
+}
