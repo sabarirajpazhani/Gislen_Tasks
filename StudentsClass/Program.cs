d99683f2f3794c288d425aa0f64900bc905ddf8e
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-27 23:49:39 +0530

    seaching the element

--- StudentsClass/Program.cs
+++ StudentsClass/Program.cs
@@ -5,14 +5,14 @@ namespace StudentsClass
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void AddStudents()
         {
-            List<Students> student = new List<Students>();
+            Dictionary<int,Students> student = new Dictionary<int,Students>();
             Console.Write("Enter the Number of Students: ");
             int num_stud = int.Parse(Console.ReadLine());
 
             int id = 100;
-            for(int i=0;i<num_stud;i++)
+            for (int i = 0; i < num_stud; i++)
             {
                 Students newStud = new Students();
                 Console.Write("Students ID: ");
@@ -28,11 +28,49 @@ namespace StudentsClass
                 Console.Write("Enter the Student Mark: ");
                 newStud.Mark = int.Parse(Console.ReadLine());
 
-                student.Add(newStud);
+                student.Add(newStud.SId,newStud);
                 Console.WriteLine("-----------------------------------------------------------");
             }
 
-            student.ForEach(x => Console.WriteLine(x.Stud_Name));
+            foreach(KeyValuePair<int, Students> i in student)
+            {
+                Console.WriteLine(i.Key +" - "+i.Value.Stud_Name);
+            }
+
+            Console.WriteLine("Want to seach the name: y/n");
+            char ch = char.Parse(Console.ReadLine());
+            if (ch == 'y')
+            {
+                SeachingByName(student);
+            }
+            else
+            {
+                Console.WriteLine("Thank You..!");
+            }
+        }
+
+        public static void SeachingByName(Dictionary<int,Students> student)
+        {
+            Console.WriteLine("Enter the Seach by Name : ");
+            string str = Console.ReadLine();
+
+            foreach(KeyValuePair<int,Students> i in student)
+            {
+                if(i.Value.Stud_Name == str)
+                {
+                    Console.WriteLine($"{i.Key} - {i.Value.Stud_Name} - {i.Value.Age} - {i.Value.Mark}");
+                }
+                else
+                {
+                    Console.WriteLine("Not Found :(");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            AddStudents();
+            
+
         }
     }
 
