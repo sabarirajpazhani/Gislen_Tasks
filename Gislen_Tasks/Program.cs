8eb864b2ef1a1c360a001069fce3a35449addb0e
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-27 20:15:39 +0530

    Reverse an array

--- Gislen_Tasks/Program.cs
+++ Gislen_Tasks/Program.cs
@@ -31,9 +31,59 @@ namespace Gislen_Tasks
                 }
             }
         }
+
+        public static void Program_Seven()
+        {
+            int n = 5;
+
+            Console.WriteLine("Enter the Give Integer Array Element : ");
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int total = 0;
+            Console.WriteLine("Reverse of Array");
+            for (int i = n - 1; i >= 0; i--)
+            {
+                total += arr[i];
+                Console.Write(arr[i] + " ");
+            }
+
+            //int ii = 0;
+            //int j = arr.Length-1;
+            //while (ii < j)
+            //{
+            //    arr[ii] ^= arr[j];
+            //    arr[j] ^= arr[ii];
+            //    arr[ii] ^= arr[j];
+
+            //    ii++;
+            //    j--;
+            //}
+
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num+" ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Average: ");
+            Console.WriteLine(total / n);
+
+        }
+
+        public static void Program_One()
+        {
+            Con
+        }
         static void Main(String[] args)
         {
-            Program_Five();
+            //Program_Five();
+            //Program_Seven();
+            Program_One();
+            Console.ReadKey();
         }
     }
 }
